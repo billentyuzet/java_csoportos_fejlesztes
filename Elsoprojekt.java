5f209217db844148055dfe603e828b7630513b48
wZs <wzs@ku>
2019-03-07 14:57:55 +0100

    java projekt

---
+++ Elsoprojekt.java
@@ -0,0 +1,13 @@
+
+package elsoprojekt;
+
+
+public class Elsoprojekt {
+
+   
+    public static void main(String[] args) {
+    System.out.print("Hello");
+    System.out.print("\n");
+    }
+    
+}
